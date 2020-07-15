49d773914702d2940ceb69da964e67f4a79da3a0
Luqmanul Hakim <arlhba@gmail.com>
2020-07-16 04:31:17 +0700

    completed course

---
+++ README.md
@@ -0,0 +1,18 @@
+## Story
+- Sebagai User saya ingin bisa menyimpan nama dan email
+- Sebagai User saya ingin bisa melihat email beserta nama yang sudah di simpan sebelum nya
+- Sebagai user saya ingin bisa meng edit nama dan email sebelum nya
+- Sebagai user saya ingin bisa menghapus email dan nama yang sudah tersimpan
+- Sebagai user saya ingin melihat total email dan nama yang telah tersimpan
+
+
+## Pelajaran yang di petik:
+- API Create
+- API Update
+- API Delete
+- API Read
+- API Count
+- Form Create
+- Form Update
+- Delete Action Web
+- Show Count Web
---
+++ main.js
@@ -0,0 +1,166 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const mysql = require('mysql');
+
+var pool = mysql.createPool({
+    connectionLimit: 10,
+    host: 'luqmanul.com',
+    user: 'root',
+    password: 'masuk123',
+    database: 'contact_manager'
+});
+
+app.use(express.json())
+app.use(express.urlencoded({
+    extended: true,
+
+}))
+
+//Serves static files (we need it to import a css file)
+app.use(express.static('public'))
+
+//Loads the handlebars module
+const handlebars = require('express-handlebars');
+
+
+//Sets handlebars configurations (we will go through them later on)
+app.engine('hbs', handlebars({
+    layoutsDir: __dirname + '/views/layouts',
+    extname: "hbs"
+}));
+
+// Sets our app to use the handlebars engine
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+    pool.query('SELECT * FROM contacts', function (error, results, fields) {
+        try {
+            if (error) throw error;
+
+
+            res.render('main', {
+                layout: 'index', contacts: results, helpers: {
+                    addOne: function (value, options) {
+                        return parseInt(value) + 1;
+                    }
+                }
+            });
+        } catch (err) {
+            res.render('main', { layout: 'error' });
+        }
+
+    });
+
+})
+
+
+app.get('/contacts', (req, res) => {
+
+    pool.query('SELECT * FROM contacts', function (error, results, fields) {
+        try {
+            if (error) throw error;
+            console.log('The solution is: ', results[0].solution);
+            res.send({ results })
+        } catch (err) {
+            handleResErr(500, res, err)
+        }
+
+    });
+
+})
+
+app.post('/contacts', (req, res) => {
+
+    const { name, email } = req.body
+
+
+    pool.query('INSERT INTO contacts (name, email) values (?, ?)', [name, email], function (error, results, fields) {
+        try {
+            if (error) throw error;
+
+            res.send({ results })
+        } catch (err) {
+            handleResErr(500, res, err)
+        }
+
+    });
+
+})
+
+app.get('/contacts/count', (req, res) => {
+
+    pool.query('SELECT COUNT(*) as count FROM contacts', function (error, results, fields) {
+        console.log(results)
+        try {
+            if (error) throw error;
+            res.send({ count: results[0]['count'] })
+        } catch (err) {
+            handleResErr(500, res, err)
+        }
+
+    });
+
+})
+
+app.get('/contacts/:id', (req, res) => {
+
+    const { id } = req.params
+
+    pool.query('SELECT * FROM contacts WHERE id = ?', [id], function (error, results, fields) {
+        try {
+            if (error) throw error;
+            res.send({ results })
+        } catch (err) {
+            handleResErr(500, res, err)
+        }
+
+    });
+
+})
+
+app.delete('/contacts/:id', (req, res) => {
+
+    const { id } = req.params
+
+    pool.query('DELETE FROM contacts WHERE id = ?', [id], function (error, results, fields) {
+        try {
+            if (error) throw error;
+            res.send({ results })
+        } catch (err) {
+            handleResErr(500, res, err)
+        }
+
+    });
+
+})
+
+app.put('/contacts/:id', (req, res) => {
+
+    const { id } = req.params
+    const { name, email } = req.body
+
+    pool.query('UPDATE contacts SET name=?, email=? WHERE id = ?', [name, email, id], function (error, results, fields) {
+        try {
+            if (error) throw error;
+            res.send({ results })
+        } catch (err) {
+            handleResErr(500, res, err)
+        }
+
+    });
+
+})
+
+
+
+const handleResErr = (statusCode, res, err) => {
+    res.status(statusCode)
+    res.send({
+        code: statusCode,
+        errMessage: err.sqlMessage
+    })
+}
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+
